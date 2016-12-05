9143afd297789c957c8b857eef72a1ea940963ec
hilukid <hilukid@uw.edu>
2016-12-04 19:38:38 -0800

    add basic search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import {Navbar, FormControl, FormGroup, Button} from 'react-bootstrap';
+import {Navbar, FormControl, FormGroup, Button, Panel} from 'react-bootstrap';
 
 import DataController from './DataController';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
 		super(props);
-		This.state = {
-			Subject: '',
-			Date: '',
-			Form: ''
+		this.state = {
+			subject: [],
+			date: '',
+			form: ''
 		};
-		This.[method] = this.[method].bind(this);
+		this.fetchContent = this.fetchContent.bind(this);
   }
 
 
   fetchContent(search) {
     var firstThis = this; // necessary??
-      DataController.____(search)
+      DataController.searchNYT(search)
       .then(function(data) {
         firstThis.setState({
-        Subject: ____;
-      }
-    )
-  })
+          subject: data.response.docs
+        }
+      )
+    })
+    console.log(this.state.subject);
   }    // ?????????
 
   render() {
@@ -39,13 +40,37 @@ class App extends Component {
         <p className="App-intro">
           Stay up to date on current events.
         </p>
-        <Navigation />
+        <Navigation getSearch={this.fetchContent} />
+        <Content getSearch={this.fetchContent} subject={this.state.subject}/>
       </div>
     );
   }
 }
 
-class Navigation extends Component {
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchValue: ""
+    };
+    this.searchClick = this.searchClick.bind(this);
+    this.searchChange = this.searchChange.bind(this);
+  }
+
+  // gets values for search bar input
+  searchClick() {
+    this.props.getSearch(this.state.searchValue);
+
+    console.log(this.state.searchValue);
+    //console.log(this.state.subject);
+  }
+
+  // grabs search bar input
+  searchChange(event) {
+    var newValue = event.target.value;
+    this.setState({searchValue: newValue});
+  }
+
   render() {
     return (
       <Navbar>
@@ -58,15 +83,44 @@ class Navigation extends Component {
         <Navbar.Collapse>
           <Navbar.Form pullLeft>
             <FormGroup>
-              <FormControl type="text" placeholder="Search" />
+              <FormControl type="text" placeholder="Search" onChange={this.searchChange}/>
             </FormGroup>
             {' '}
-            <Button type="submit">Submit</Button>
+            <Button type="submit" onClick={this.searchClick}>Submit</Button>
           </Navbar.Form>
         </Navbar.Collapse>
       </Navbar>
     );
   }
 }
+
+class Content extends React.Component {
+  render() {
+    var articles = this.props.subject.map(function(art) {
+      return <Card art={art} key={art._id}/>;
+    });
+
+    return (
+      <div role= "main" className="contentArea"> 
+        {articles}
+      </div>
+    );
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="artCard">
+        <Panel className="songList" header={this.props.art.headline.main}>
+          <img className="albmImg"  alt="thumbnail"/>
+          <p>{this.props.art.snippet}</p>
+        </Panel>
+      </div>
+    );
+  }
+}
+
+//src={this.props.art.multimedia[0].url}
 
 export default App;
--- src/DataController.js
+++ src/DataController.js
@@ -1,9 +1,14 @@
 var baseApiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 var apiKey = 'ea2962f859ad4ef4b1e89386e60a9b94';
 
+// ex: https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=ea2962f859ad4ef4b1e89386e60a9b94&q=black+lives+matter
+
 var controller = {
-    searchTracks: function (query) {
-        var trackUri = baseApiUrl + "?q=" + query.split(" ").join("+") + "&api-key" + apiKey;
+    searchNYT: function(query) {
+        var trackUri = baseApiUrl + "?api-key=" + apiKey + "&q=" + query.split(" ").join("+");
+
+        console.log(trackUri);
+
         return fetch(trackUri)
             .then(function (res) { return res.json() })
     }
