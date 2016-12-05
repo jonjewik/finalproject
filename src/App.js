d2ec2251a6560d2b6477e8758c5f91070ca03ea8
markosmezgebu <markosm@uw.edu>
2016-12-04 23:47:24 -0800

    Add date search

--- src/App.js
+++ src/App.js
@@ -2,75 +2,112 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import {Navbar, FormControl, FormGroup, Button, Panel} from 'react-bootstrap';
-
 import DataController from './DataController';
-
 class App extends React.Component {
   constructor(props) {
-		super(props);
-		this.state = {
-			subject: [],
-			date: '',
-			form: ''
-		};
-		this.fetchContent = this.fetchContent.bind(this);
+    super(props);
+    this.state = {
+      subject: [],
+      date: '',
+      form: ''
+    };
+    this.fetchContent = this.fetchContent.bind(this);
+    this.setDate = this.setDate.bind(this);
   }
-
-
   fetchContent(search) {
     var firstThis = this; // necessary??
+    if (this.state.date !== "") {
+      DataController.dateSearch(search, this.state.date)
+        .then(function (data) {
+          firstThis.setState({
+            subject: data.response.docs
+          })
+        })
+    } else {
       DataController.searchNYT(search)
-      .then(function(data) {
-        firstThis.setState({
-          subject: data.response.docs
-        }
-      )
-    })
-    console.log(this.state.subject);
-  }    // ?????????
+        .then(function (data) {
+          firstThis.setState({
+            subject: data.response.docs
+          })
+        })
+    }
 
+    console.log(this.state.subject);
+    console.log(this.state.date);
+  }    // ?????????
+  // fetchFilteredContent(search) {
+  //   var firstThis = this; // necessary??
+  //     DataController.searchNYT(search)
+  //     .then(function(data) {
+  //       firstThis.setState({
+  //         subject: data.response.docs
+  //       }
+  //     )
+  //   })
+  //   console.log(this.state.subject);
+  // }
+  setDate(newDate) {
+    console.log('first' + newDate);
+    // var firstThis = this;
+    // firstThis.setState({
+    //  date: date
+    // })
+    this.setState({ date: newDate })
+    //this.fetchContent = this.fetchContent.bind(this);
+    console.log("setDate");
+    console.log("NOW " + this.state.date);
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-        <span><i className="fa fa-newspaper-o fa-5x App-logo" aria-hidden="true"></i></span>
-
+          <span><i className="fa fa-newspaper-o fa-5x App-logo" aria-hidden="true"></i></span>
           <h2>[untitled social justice resource center]</h2>
         </div>
         <p className="App-intro">
           Stay up to date on current events.
         </p>
-        <Navigation getSearch={this.fetchContent} />
+        <Navigation getSearch={this.fetchContent} getDate={this.setDate}/>
         <Content getSearch={this.fetchContent} subject={this.state.subject}/>
       </div>
     );
   }
 }
-
 class Navigation extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchValue: ""
+      searchValue: "",
+      date: ""
     };
     this.searchClick = this.searchClick.bind(this);
     this.searchChange = this.searchChange.bind(this);
+    this.startDate = this.startDate.bind(this);
   }
-
   // gets values for search bar input
   searchClick() {
-    this.props.getSearch(this.state.searchValue);
-
-    console.log(this.state.searchValue);
+    console.log(this.state.date);
+    if (this.state.date !== "") {
+      this.props.getDate(this.state.date);
+      this.props.getSearch(this.state.searchValue);
+    } else {
+      this.props.getSearch(this.state.searchValue);
+    }
+    // this.props.getDate(this.state.date);
+    // this.props.getSearch(this.state.searchValue);
+    //console.log(this.state.searchValue);
     //console.log(this.state.subject);
   }
-
   // grabs search bar input
   searchChange(event) {
     var newValue = event.target.value;
-    this.setState({searchValue: newValue});
+    this.setState({ searchValue: newValue });
   }
+  startDate(event) {
+    var newDate = event.target.value;
+    this.setState({ date: newDate });
 
+  }
   render() {
     return (
       <Navbar>
@@ -86,6 +123,9 @@ class Navigation extends React.Component {
               <FormControl type="text" placeholder="Search" onChange={this.searchChange}/>
             </FormGroup>
             {' '}
+            <FormGroup>
+              <FormControl type="text" placeholder="YYYYMMDD" onChange={this.startDate}/>
+            </FormGroup>
             <Button type="submit" onClick={this.searchClick}>Submit</Button>
           </Navbar.Form>
         </Navbar.Collapse>
@@ -93,21 +133,18 @@ class Navigation extends React.Component {
     );
   }
 }
-
 class Content extends React.Component {
   render() {
-    var articles = this.props.subject.map(function(art) {
+    var articles = this.props.subject.map(function (art) {
       return <Card art={art} key={art._id}/>;
     });
-
     return (
-      <div role= "main" className="contentArea"> 
+      <div role= "main" className="contentArea">
         {articles}
       </div>
     );
   }
 }
-
 class Card extends React.Component {
   render() {
     return (
@@ -120,7 +157,5 @@ class Card extends React.Component {
     );
   }
 }
-
 //src={this.props.art.multimedia[0].url}
-
 export default App;
