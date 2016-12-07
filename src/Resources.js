524cc9f865de9c94a4964fff6718fc5d9811e916
Anna Lim <lalim12@uw.edu>
2016-12-06 22:41:20 -0800

    added advice

--- src/Resources.js
+++ src/Resources.js
@@ -5,74 +5,89 @@ class Resources extends React.Component {
   render() {
     return (
       <div>
-      <h3 role="banner"> Resources </h3>
-      <h4> LGBTQ </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.hrusa.org/default.shtm">Human Rights Resource Center</a></li>
-          <li><a href="http://www.equalityfederation.org/">Equality Federation</a></li>
-          <li><a href="http://hrc.org/">Human Rights Campaign (HRC)</a></li>
-          <li><a href="http://www.biresource.net/">Bisexual Resource Center</a></li>
-          <li><a href="http://www.aclu.org/lgbt-rights">American Civil Liberties Union (ACLU)</a></li>
-        </ul>
-      </p>
+        <h3 role="banner"> Resources </h3>
+        <h4> LGBTQ </h4>
+        <p><em>Know you are not alone:</em> LGBTQ+ people around the world have been through the same struggle. Check out the stories of hope and recovery at the bottom of the page to see how they cope.</p>
+        <p><em>Build your support network:</em> Find allies in your life who will help keep you safe and who you can lean on if you feel depressed or suicidal.</p>
+        <p><em>Talk to someone:</em> Silence isn’t strength. Don’t keep suicidal feelings to yourself. Lean on your support network, find a therapist or a support group.</p>
+        <p><em>Make a safety plan:</em> Have a step-by-step plan ready for if/when you feel depressed, suicidal, or in crisis, so you can start at step one and continue through the steps until you feel safe.</p>
+    
+        <p>
+          <ul>
+            <li><a href="http://www.hrusa.org/default.shtm">Human Rights Resource Center</a></li>
+            <li><a href="http://www.equalityfederation.org/">Equality Federation</a></li>
+            <li><a href="http://hrc.org/">Human Rights Campaign (HRC)</a></li>
+            <li><a href="http://www.biresource.net/">Bisexual Resource Center</a></li>
+            <li><a href="http://www.aclu.org/lgbt-rights">American Civil Liberties Union (ACLU)</a></li>
+          </ul>
+        </p>
 
-      <h4 role="banner"> Equality </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.glbthotline.org/hotline.html">GLBT Hotline: <a href="tel:18888434564">1-888-843-4564</a></a></li>
-          <li><a href="http://www.aclu.org">American Civil Liberties Union (ACLU)</a></li>
-          <li><a href="http://www.racialequityresourceguide.org">Racial Equity Resource Guide</a></li>
-          <li><a href="https://www.racialequitytools.org/home">Racial Equity Tools</a></li>
-          <li><a href="https://www2.ed.gov/policy/rights/guid/ocr/sex.html">Sex Discrimination Resources</a></li>
-        </ul>
-      </p>
-      <h4 role="banner"> Youth </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.pbs.org/wnet/cryforhelp/episodes/resources/hotlines-and-web-sites-for-teens/?p=11">National Suicide Hotline: <a href="tel:+18007842433">1-800-SUICIDE (784-2433)</a></a></li>
-          <li><a href="http://nationalsafeplace.org">Safe Place</a></li>
-          <li><a href="http://www.pbs.org/wnet/cryforhelp/episodes/resources/hotlines-and-web-sites-for-teens/?p=11">PBS Teenage Mental Illness and Suicide Help</a></li>
-          <li><a href="https://www.jedfoundation.org">The Jed Foundation</a></li>
-        </ul>
-      </p>
-      <h4 role="banner"> Abuse Victims </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.thehotline.org/resources/">National Domestic Violence Hotline: <a href="tel:+18007997233">1-800-799-7233</a></a></li>
-          <li><a href="http://www.stopstreetharassment.org/our-work/nationalshhotline/">National Street Harassment Hotline: <a href="tel:+8558975910">855-897-5910</a></a></li>
-          <li><a href="http://www.childhelp.org/">National Child Abuse Hotline: <a href="tel:8004222253">800-422-2253</a></a></li>
-          <li><a href="http://www.ncadv.org/">National Coalition against Domestic Violence</a></li>
-          <li><a href="http://www.notalone.gov/">NotAlone.gov</a></li>
-        </ul>
-      </p>
+        <h4 role="banner"> Equality </h4>
+        <p>
+          <ul>
+            <li><a href="http://www.glbthotline.org/hotline.html">GLBT Hotline: <a href="tel:18888434564">1-888-843-4564</a></a></li>
+            <li><a href="http://www.aclu.org">American Civil Liberties Union (ACLU)</a></li>
+            <li><a href="http://www.racialequityresourceguide.org">Racial Equity Resource Guide</a></li>
+            <li><a href="https://www.racialequitytools.org/home">Racial Equity Tools</a></li>
+            <li><a href="https://www2.ed.gov/policy/rights/guid/ocr/sex.html">Sex Discrimination Resources</a></li>
+          </ul>
+        </p>
+        <h4 role="banner"> Youth </h4>
+        <p><em>Ask for help:</em> Don’t be afraid to let your friends, family, or teachers know what you need when they ask; they want to help. You can also call the National Suicide Prevention Lifeline any time — calls are confidential.</p>
+        <p><em>Remember that this feeling can be overcome:</em> Suicide is a permanent solution to a temporary problem. Family conflict, relationships, grades, sexual identity, and the loss of important people can seem impossible to deal with. But with support from others, you can.</p>
+        <p><em>Evaluate the relationships in your life:</em> Love and friendship are all about respect. Toxic or unhealthy relationships can negatively affect you. Whether you’re dating or building new friendships, remember your rights.</p>
+        <p>
+          <ul>
+            <li><a href="http://www.pbs.org/wnet/cryforhelp/episodes/resources/hotlines-and-web-sites-for-teens/?p=11">National Suicide Hotline: <a href="tel:+18007842433">1-800-SUICIDE (784-2433)</a></a></li>
+            <li><a href="http://nationalsafeplace.org">Safe Place</a></li>
+            <li><a href="http://www.pbs.org/wnet/cryforhelp/episodes/resources/hotlines-and-web-sites-for-teens/?p=11">PBS Teenage Mental Illness and Suicide Help</a></li>
+            <li><a href="https://www.jedfoundation.org">The Jed Foundation</a></li>
+          </ul>
+        </p>
 
-      <h4 role="banner"> Rape and Sexual Assault </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.rainn.org/get-help/national-sexual-assault-hotline">National Sexual Assault Hotline: <a href="tel:+18006564673">1-800-656-HOPE</a></a></li>
-          <li><a href="http://www.nsvrc.org/">National Sexual Violence Resource Center</a></li>
-          <li><a href="http://nsopw.gov/Core/Conditions.aspx">National Sex Offender Registry</a></li>
-        </ul>
-      </p>
+        <h4 role="banner"> Rape and Sexual Assault </h4>
+        <p>
+          <ul>
+            <li><a href="http://www.rainn.org/get-help/national-sexual-assault-hotline">National Sexual Assault Hotline: <a href="tel:+18006564673">1-800-656-HOPE</a></a></li>
+            <li><a href="http://www.nsvrc.org/">National Sexual Violence Resource Center</a></li>
+            <li><a href="http://nsopw.gov/Core/Conditions.aspx">National Sex Offender Registry</a></li>
+          </ul>
+        </p>
 
-      <h4 role="banner"> Muslims </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.mrcssi.com">Muslim Resource Center</a></li>
-          <li><a href="http://www.api-gbv.org/organizing/manadv.php">Muslim Advocacy Network</a></li>
-          <li><a href="http://www.mwrcnfp.org">Muslim Women Resource Center</a></li>
-        </ul>
-      </p>
+        <h4 role="banner"> Abuse Victims </h4>
+        <p><em>Find A Therapist/Support Group:</em> Speaking to someone, whether by going to a therapist or by attending a support group, can help you feel better and improve your mental health. These resources can help you find a psychologist, psychiatrist, or support group near you.</p>
+        <p><em>Build A Support Network:</em> You don't have to deal with crisis on your own. Those you choose to confide in can provide encouragement and help you through a crisis.</p>
+        <p><em>Use Your Support Network:</em> Leaning on your support network can help you cope during difficult moments and is an important step in getting help and moving forward.</p>
+        <p><em>Contact family members or friends who may help to resolve a crisis:</em> Make a list of people who are supportive and who you feel you can talk to when under stress.</p>
+        
+        <p>
+          <ul>
+            <li><a href="http://www.thehotline.org/resources/">National Domestic Violence Hotline: <a href="tel:+18007997233">1-800-799-7233</a></a></li>
+            <li><a href="http://www.stopstreetharassment.org/our-work/nationalshhotline/">National Street Harassment Hotline: <a href="tel:+8558975910">855-897-5910</a></a></li>
+            <li><a href="http://www.childhelp.org/">National Child Abuse Hotline: <a href="tel:8004222253">800-422-2253</a></a></li>
+            <li><a href="http://www.ncadv.org/">National Coalition against Domestic Violence</a></li>
+            <li><a href="http://www.notalone.gov/">NotAlone.gov</a></li>
+          </ul>
+        </p>
 
-      <h4 role="banner"> Women </h4>
-      <p>
-        <ul>
-          <li><a href="http://www.aclu.org">American Civil Liberties Union (ACLU)</a></li>
-          <li><a href="http://www.learningpartnership.org/womens-human-rights-resources">Women's Learning Partnership</a></li>
-          <li><a href="https://www.awid.org/priority-areas/resourcing-womens-rights">Resourcing Womens Rights</a></li>
-        </ul>
-      </p>
+        <h4 role="banner"> Muslims </h4>
+        <p>
+          <ul>
+            <li><a href="http://www.mrcssi.com">Muslim Resource Center</a></li>
+            <li><a href="http://www.api-gbv.org/organizing/manadv.php">Muslim Advocacy Network</a></li>
+            <li><a href="http://www.mwrcnfp.org">Muslim Women Resource Center</a></li>
+          </ul>
+        </p>
+
+        <h4 role="banner"> Women </h4>
+        <p>
+          <ul>
+            <li><a href="http://www.aclu.org">American Civil Liberties Union (ACLU)</a></li>
+            <li><a href="http://www.learningpartnership.org/womens-human-rights-resources">Women's Learning Partnership</a></li>
+            <li><a href="https://www.awid.org/priority-areas/resourcing-womens-rights">Resourcing Womens Rights</a></li>
+          </ul>
+        </p>
+        <p class="copyright"> Advice taken from: <a href="https://suicidepreventionlifeline.org">https://suicidepreventionlifeline.org</a></p>
       </div>
 
     );
