16981707e89673d996a09c02a46b431c5e9115da
Anna Lim <lalim12@uw.edu>
2016-12-06 20:43:54 -0800

    added routing

--- src/Resources.js
+++ src/Resources.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import _ from 'lodash';
 import { Carousel, Button } from 'react-bootstrap';
 
 class Resources extends React.Component {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import Resources from './Resources';
+
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import DataController from './DataController';
 
-ReactDOM.render(
+/*ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+);*/
 
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="resources" component={Resources} />
+    </Route>
+  </Router>,
+  document.getElementById('root')
+);
